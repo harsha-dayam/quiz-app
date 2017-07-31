c002a120ab43e279249016c23365873962881ba2
Harsha Dayam <harshavardhan2506@gmail.com>
2017-07-31 16:38:54 -0500

    First commit

---
+++ js/quiz.js
@@ -0,0 +1,53 @@
+var quiz={
+	"topics":[{
+		"name":"Topic 1",
+		"picture":"http://via.placeholder.com/100x100",
+		"description":"Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui.",
+		"questions":[{
+			"q":"How days are there in a year?",
+			"options":["300","360","365","367"]
+		},
+		{
+            "q":"How hours are there in a day?",
+			"options":["25","24","12","20"]
+		},
+		{
+			"q":"How minutes are there in an hour?",
+			"options":["60","30","65","36"]
+		}]
+	},
+	{
+		"name":"Topic 2",
+		"picture":"http://via.placeholder.com/100x100",
+		"description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		"questions":[{
+			"q":"How days are there in a year?",
+			"options":["300","360","365","367"]
+		},
+		{
+            "q":"How hours are there in a day?",
+			"options":["25","24","12","20"]
+		},
+		{
+			"q":"How minutes are there in an hour?",
+			"options":["60","30","65","36"]
+		}]
+	},
+	{
+		"name":"Topic 3",
+		"picture":"http://via.placeholder.com/100x100",
+		"description":"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ullamco laboris.",
+		"questions":[{
+			"q":"How days are there in a year?",
+			"options":["300","360","365","367"]
+		},
+		{
+            "q":"How hours are there in a day?",
+			"options":["25","24","12","20"]
+		},
+		{
+			"q":"How minutes are there in an hour?",
+			"options":["60","30","65","36"]
+		}]
+	}]
+}
