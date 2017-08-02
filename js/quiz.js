bd1b33d3b74c55c9363b9ecb74fd9ca79d1239d6
Harsha Dayam <harshavardhan2506@gmail.com>
2017-08-02 18:32:47 -0500

    Submit/Previous buttons. Navigate to topics from landing

--- js/quiz.js
+++ js/quiz.js
@@ -4,16 +4,19 @@ var quiz={
 		"picture":"http://via.placeholder.com/100x100",
 		"description":"Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui.",
 		"questions":[{
-			"q":"How days are there in a year?",
-			"options":["300","360","365","367"]
+			"q":"How days are there in a year ?",
+			"options":["300","360","365","367"],
+			"answer":"2"
 		},
 		{
-            "q":"How hours are there in a day?",
-			"options":["25","24","12","20"]
+            "q":"How hours are there in a day ?",
+			"options":["25","24","12","20"],
+			"answer":"1"
 		},
 		{
-			"q":"How minutes are there in an hour?",
-			"options":["60","30","65","36"]
+			"q":"How minutes are there in an hour ?",
+			"options":["60","30","65","36"],
+			"answer":"0"
 		}]
 	},
 	{
@@ -21,16 +24,19 @@ var quiz={
 		"picture":"http://via.placeholder.com/100x100",
 		"description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 		"questions":[{
-			"q":"How days are there in a year?",
-			"options":["300","360","365","367"]
+			"q":"What is the capital of Turkey ?",
+			"options":["Istanbul","Ankara","Adana","Antalya"],
+			"answer":"1"
 		},
 		{
-            "q":"How hours are there in a day?",
-			"options":["25","24","12","20"]
+            "q":"What is the capital of Germany ?",
+			"options":["Hamburg","Berlin","Munich","Frankfurt"],
+			"answer":"1"
 		},
 		{
-			"q":"How minutes are there in an hour?",
-			"options":["60","30","65","36"]
+			"q":"What is the capital of Brazil ?",
+			"options":["Salvador","Sao Paulo","Brasilia","Rio de Janeiro"],
+			"answer":"2"
 		}]
 	},
 	{
@@ -38,16 +44,19 @@ var quiz={
 		"picture":"http://via.placeholder.com/100x100",
 		"description":"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ullamco laboris.",
 		"questions":[{
-			"q":"How days are there in a year?",
-			"options":["300","360","365","367"]
+			"q":"What is the unit for measuring the amplitude of sound ?",
+			"options":["Decibel","Coulomb","Hum","Cycles"],
+			"answer":"0"
 		},
 		{
-            "q":"How hours are there in a day?",
-			"options":["25","24","12","20"]
+            "q":"Reading of a barometer going down is an indication of ____",
+			"options":["snow","storm","intense heat","rainfall"],
+			"answer":"3"
 		},
 		{
-			"q":"How minutes are there in an hour?",
-			"options":["60","30","65","36"]
+			"q":"Fathom is the unit of ____",
+			"options":["sound","depth","frequency","distance"],
+			"answer":"1"
 		}]
 	}]
 }
