45430fad41a99696a2d26eeb7b6ff3d9a24a4d98
Harsha Dayam <harshavardhan2506@gmail.com>
2017-08-06 02:56:07 -0500

    Store answer values. Go to respective topic

---
+++ js/object.js
@@ -0,0 +1,62 @@
+var quiz={
+	"topics":[{
+		"name":"Topic 1",
+		"picture":"http://via.placeholder.com/100x100",
+		"description":"Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui.",
+		"questions":[{
+			"q":"How days are there in a year ?",
+			"options":["300","360","365","367"],
+			"answer":"2"
+		},
+		{
+            "q":"How hours are there in a day ?",
+			"options":["25","24","12","20"],
+			"answer":"1"
+		},
+		{
+			"q":"How minutes are there in an hour ?",
+			"options":["60","30","65","36"],
+			"answer":"0"
+		}]
+	},
+	{
+		"name":"Topic 2",
+		"picture":"http://via.placeholder.com/100x100",
+		"description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		"questions":[{
+			"q":"What is the capital of Turkey ?",
+			"options":["Istanbul","Ankara","Adana","Antalya"],
+			"answer":"1"
+		},
+		{
+            "q":"What is the capital of Germany ?",
+			"options":["Hamburg","Berlin","Munich","Frankfurt"],
+			"answer":"1"
+		},
+		{
+			"q":"What is the capital of Brazil ?",
+			"options":["Salvador","Sao Paulo","Brasilia","Rio de Janeiro"],
+			"answer":"2"
+		}]
+	},
+	{
+		"name":"Topic 3",
+		"picture":"http://via.placeholder.com/100x100",
+		"description":"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ullamco laboris.",
+		"questions":[{
+			"q":"What is the unit for measuring the amplitude of sound ?",
+			"options":["Decibel","Coulomb","Hum","Cycles"],
+			"answer":"0"
+		},
+		{
+            "q":"Reading of a barometer going down is an indication of ____",
+			"options":["snow","storm","intense heat","rainfall"],
+			"answer":"3"
+		},
+		{
+			"q":"Fathom is the unit of ____",
+			"options":["sound","depth","frequency","distance"],
+			"answer":"1"
+		}]
+	}]
+}
--- js/quiz.js
+++ js/quiz.js
@@ -1,62 +1,63 @@
-var quiz={
-	"topics":[{
-		"name":"Topic 1",
-		"picture":"http://via.placeholder.com/100x100",
-		"description":"Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui.",
-		"questions":[{
-			"q":"How days are there in a year ?",
-			"options":["300","360","365","367"],
-			"answer":"2"
-		},
-		{
-            "q":"How hours are there in a day ?",
-			"options":["25","24","12","20"],
-			"answer":"1"
-		},
-		{
-			"q":"How minutes are there in an hour ?",
-			"options":["60","30","65","36"],
-			"answer":"0"
-		}]
-	},
-	{
-		"name":"Topic 2",
-		"picture":"http://via.placeholder.com/100x100",
-		"description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		"questions":[{
-			"q":"What is the capital of Turkey ?",
-			"options":["Istanbul","Ankara","Adana","Antalya"],
-			"answer":"1"
-		},
-		{
-            "q":"What is the capital of Germany ?",
-			"options":["Hamburg","Berlin","Munich","Frankfurt"],
-			"answer":"1"
-		},
-		{
-			"q":"What is the capital of Brazil ?",
-			"options":["Salvador","Sao Paulo","Brasilia","Rio de Janeiro"],
-			"answer":"2"
-		}]
-	},
-	{
-		"name":"Topic 3",
-		"picture":"http://via.placeholder.com/100x100",
-		"description":"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ullamco laboris.",
-		"questions":[{
-			"q":"What is the unit for measuring the amplitude of sound ?",
-			"options":["Decibel","Coulomb","Hum","Cycles"],
-			"answer":"0"
-		},
-		{
-            "q":"Reading of a barometer going down is an indication of ____",
-			"options":["snow","storm","intense heat","rainfall"],
-			"answer":"3"
-		},
-		{
-			"q":"Fathom is the unit of ____",
-			"options":["sound","depth","frequency","distance"],
-			"answer":"1"
-		}]
-	}]
+var qno = 0;
+var answers = [];
+var temp="";
+var qVar = quiz.topics[localStorage.getItem('topicNo')];
+renderQ();
+function nextQ(){
+  if(qno<qVar.questions.length-1){
+    qno++;
+    renderQ();
+  }
 }
+function prevQ(){
+  if(qno>0){
+    qno--;
+    renderQ();
+  }
+}
+function renderQ(){
+  temp = '<div class="well"><h2 class="text-center">'+qVar.questions[qno].q+'</h2></div><div class="row">';
+  for (var i = 0; i < qVar.questions[qno].options.length; i++) {
+    // temp+='<div class="col-md-6"><p class="text-center"><label class="btn btn-default btn-lg"><input type="radio" value="'+qVar.questions[qno].options[i]+'" name="options" onClick="selectOption(this)"> '+qVar.questions[qno].options[i]+'</input></label></p></div>';
+    temp+='<div class="col-md-6"><p class="text-center"><input class="btn btn-default btn-lg" value="'+qVar.questions[qno].options[i]+'" onClick="selectOption(this)"></p></div>';
+  }
+  temp+="</div>";
+  document.getElementById("question").innerHTML=temp;
+  if(qno == 0) {
+    document.getElementById("previous").disabled = true;
+  } else if (qno==qVar.questions.length-1){
+    document.getElementsByClassName("next")[0].style.display = "none";
+    document.getElementById("submit").style.display = "block";
+  }
+  else {
+    document.getElementsByClassName("next")[0].style.display = "block";
+    document.getElementById("submit").style.display = "none";
+    document.getElementById("previous").disabled = false;
+  }
+}
+
+function selectOption(obj){
+  answers[qno] = obj.value;
+  // document.getElementById("skip").disabled = true;
+
+}
+// var i;
+// function skipQ(){
+//   i = qno;
+//   while(true){
+//     if(i<qVar.questions.length-1 && answers[i] === undefined ){nextQ(); break;}
+//     else if (qno==0){break;}
+//     else {qno=0; renderQ();}
+
+//     // if (i==qno && ){ console.log("last question"); break;}
+//   }
+// }
+
+
+//   $('.btn-default').on('click', 'label.btn', function(e) {
+//     if ($(this).hasClass('active')) {
+//       setTimeout(function() {
+//         $(this).removeClass('active').find('input').prop('checked', false);
+//       }.bind(this), 10);
+//     }
+//   });
