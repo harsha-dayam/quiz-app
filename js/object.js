082b28adf353ea1c33696597641930ef71cfe2d2
Harsha Dayam <harshavardhan2506@gmail.com>
2017-08-15 07:18:39 -0500

    Version with a timer for all questions together.

--- js/object.js
+++ js/object.js
@@ -4,19 +4,19 @@ var quiz={
 		"picture":"http://via.placeholder.com/100x100",
 		"description":"Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui. Donec sed odio dui.",
 		"questions":[{
-			"q":"How days are there in a year ?",
+			"q":"How many days are there in a year ?",
 			"options":["300","360","365","367"],
-			"answer":"2"
+			"answer":"365"
 		},
 		{
-            "q":"How hours are there in a day ?",
+            "q":"How many hours are there in a day ?",
 			"options":["25","24","12","20"],
-			"answer":"1"
+			"answer":"24"
 		},
 		{
-			"q":"How minutes are there in an hour ?",
+			"q":"How many minutes are there in an hour ?",
 			"options":["60","30","65","36"],
-			"answer":"0"
+			"answer":"60"
 		}]
 	},
 	{
@@ -26,17 +26,17 @@ var quiz={
 		"questions":[{
 			"q":"What is the capital of Turkey ?",
 			"options":["Istanbul","Ankara","Adana","Antalya"],
-			"answer":"1"
+			"answer":"Ankara"
 		},
 		{
             "q":"What is the capital of Germany ?",
 			"options":["Hamburg","Berlin","Munich","Frankfurt"],
-			"answer":"1"
+			"answer":"Berlin"
 		},
 		{
 			"q":"What is the capital of Brazil ?",
 			"options":["Salvador","Sao Paulo","Brasilia","Rio de Janeiro"],
-			"answer":"2"
+			"answer":"Brasilia"
 		}]
 	},
 	{
@@ -46,17 +46,17 @@ var quiz={
 		"questions":[{
 			"q":"What is the unit for measuring the amplitude of sound ?",
 			"options":["Decibel","Coulomb","Hum","Cycles"],
-			"answer":"0"
+			"answer":"Decibel"
 		},
 		{
             "q":"Reading of a barometer going down is an indication of ____",
 			"options":["snow","storm","intense heat","rainfall"],
-			"answer":"3"
+			"answer":"rainfall"
 		},
 		{
 			"q":"Fathom is the unit of ____",
 			"options":["sound","depth","frequency","distance"],
-			"answer":"1"
+			"answer":"depth"
 		}]
 	}]
 }
--- js/quiz.js
+++ js/quiz.js
@@ -1,11 +1,60 @@
 var qno = 0;
+if(sessionStorage.getItem("questionNo")) qno = sessionStorage.getItem("questionNo");
+var qVar = quiz.topics[sessionStorage.getItem('topicNo')];
 var answers = [];
+if(sessionStorage.getItem(qVar.name)) answers=sessionStorage.getItem(qVar.name).split(",");
 var temp="";
-var qVar = quiz.topics[localStorage.getItem('topicNo')];
+
+/* Timer */
+var timeLimit = (qVar.questions.length)*60000;
+if(sessionStorage.getItem("dist")) timeLimit = sessionStorage.getItem("dist");
+// function foo() {   
+//     timeLimit -= 1000;
+//     var minutes = Math.floor((timeLimit % (1000 * 60 * 60)) / (1000 * 60));
+//     minutes = ("0" + minutes).slice(-2);
+//     var seconds = Math.floor((timeLimit % (1000 * 60)) / 1000);
+//     seconds = ("0" + seconds).slice(-2);
+//     document.getElementById("time").innerHTML = minutes + ":" + seconds;
+
+//     if (timeLimit < 0) {       
+//         alert("timeup");
+//         nextQ();
+//         timeLimit=6000;
+//     }
+//    setTimeout(foo, 1000);
+// }
+// foo();
+
+var timer = setInterval(function() {
+  
+  drawHand();
+  timeLimit -= 1000;
+
+  var minutes = Math.floor((timeLimit % (1000 * 60 * 60)) / (1000 * 60));
+  minutes = ("0" + minutes).slice(-2);
+  var seconds = Math.floor((timeLimit % (1000 * 60)) / 1000);
+  seconds = ("0" + seconds).slice(-2);
+  
+  document.getElementById("time").innerHTML = minutes + ":" + seconds;
+  //document.getElementById("time").style.display = "inline-block";
+
+  if (timeLimit < 0) {
+    clearInterval(timer);
+    document.getElementById("time").innerHTML = "00:00";
+    document.getElementById("topModal").style.display = "block";
+  }
+}, 1000);
+
+document.getElementById("topic").innerHTML = qVar.name;
 renderQ();
+
 function nextQ(){
   if(qno<qVar.questions.length-1){
     qno++;
+    renderQ();
+  }
+  else{
+    qno=0;
     renderQ();
   }
 }
@@ -14,50 +63,195 @@ function prevQ(){
     qno--;
     renderQ();
   }
+  else if(qno==0 && answers.length>1){
+    qno=answers.length-1;
+    renderQ();
+  }
 }
 function renderQ(){
+
+  /*rendering question and answers*/
   temp = '<div class="well"><h2 class="text-center">'+qVar.questions[qno].q+'</h2></div><div class="row">';
   for (var i = 0; i < qVar.questions[qno].options.length; i++) {
-    // temp+='<div class="col-md-6"><p class="text-center"><label class="btn btn-default btn-lg"><input type="radio" value="'+qVar.questions[qno].options[i]+'" name="options" onClick="selectOption(this)"> '+qVar.questions[qno].options[i]+'</input></label></p></div>';
-    temp+='<div class="col-md-6"><p class="text-center"><input class="btn btn-default btn-lg" value="'+qVar.questions[qno].options[i]+'" onClick="selectOption(this)"></p></div>';
+    temp+='<div class="col-sm-6"><p class="text-center"><label class="btn btn-default btn-lg options-'+qno+'"><input type="radio" value="'+qVar.questions[qno].options[i]+'" name="options" onChange="selectOption(this)"> '+qVar.questions[qno].options[i]+'</input></label></p></div>';
+    // temp+='<div class="col-md-6"><p class="text-center"><input class="btn btn-default btn-lg" value="'+qVar.questions[qno].options[i]+'" onClick="selectOption(this)"></p></div>';
   }
   temp+="</div>";
   document.getElementById("question").innerHTML=temp;
-  if(qno == 0) {
+  
+  /*disabling skip button*/
+  if(answers[qno]!==undefined){document.getElementById("skip").disabled = true;}
+  /*disabling/enabling next button*/
+  if(qno==0 && answers.length==0){document.getElementById("next").disabled = true;}
+  else{document.getElementById("next").disabled = false;}
+  /*disabling/enabling previous, skip, next & submit*/
+  if(qno == 0 && answers.length < 2) {
     document.getElementById("previous").disabled = true;
-  } else if (qno==qVar.questions.length-1){
+  }
+  else if (answers.length==qVar.questions.length && allAnswered()==true){
     document.getElementsByClassName("next")[0].style.display = "none";
     document.getElementById("submit").style.display = "block";
+    document.getElementById("skip").disabled = true;
   }
   else {
     document.getElementsByClassName("next")[0].style.display = "block";
     document.getElementById("submit").style.display = "none";
     document.getElementById("previous").disabled = false;
+    document.getElementById("skip").disabled = false;
+  }
+  //focus on selected answer
+  if(answers[qno]!==undefined){
+    document.querySelectorAll('[value="'+answers[qno]+'"]')[0].parentNode.classList.add("active");
   }
 }
 
 function selectOption(obj){
   answers[qno] = obj.value;
-  // document.getElementById("skip").disabled = true;
-
+  //skip button
+  if(answers[qno]!==undefined){document.getElementById("skip").disabled = true;}
+  else{document.getElementById("skip").disabled = false;}
+  //next button
+  if (answers.length>0){document.getElementById("next").disabled = false;}
+  //submit button
+  if(answers.length==qVar.questions.length && allAnswered()==true){
+    document.getElementsByClassName("next")[0].style.display = "none";
+    document.getElementById("submit").style.display = "block";
+  }
+  var opt=document.getElementsByClassName('options-'+qno+'');
+  for(var i=0; i<opt.length; i++){
+    opt[i].classList.remove("active");
+  }
+  obj.parentNode.classList.add("active");
 }
-// var i;
-// function skipQ(){
-//   i = qno;
-//   while(true){
-//     if(i<qVar.questions.length-1 && answers[i] === undefined ){nextQ(); break;}
-//     else if (qno==0){break;}
-//     else {qno=0; renderQ();}
 
-//     // if (i==qno && ){ console.log("last question"); break;}
-//   }
-// }
+function allAnswered(){
+  var count = 0;
+  for (var i=0; i<answers.length; i++){
+    if (answers[i] === undefined){count++;}
+  }
+  if (count>0){return false}
+    else {return true}
+  }
 
 
-//   $('.btn-default').on('click', 'label.btn', function(e) {
-//     if ($(this).hasClass('active')) {
-//       setTimeout(function() {
-//         $(this).removeClass('active').find('input').prop('checked', false);
-//       }.bind(this), 10);
-//     }
-//   });
+function skipQ(){
+  if (answers.length==0){
+   if(qno<qVar.questions.length-1){nextQ();}
+   else {qno=0; renderQ();}
+ }
+ else{
+  var i;
+  if (qno==qVar.questions.length-1){i=0;}
+  else {i=qno+1;}
+  while(true){
+    if(i==qno){alert("You've reached the last question!"); break;}
+    else if(answers[i]===undefined){qno=i; renderQ(); break;}
+    else if(i==qVar.questions.length-1){i=0;}
+    else {i++;}
+  }
+}  
+}
+
+function validateAns(){
+
+  clearInterval(timer);
+  document.getElementById("time").innerHTML = "00:00"; 
+  var count = 0;
+  for(var i in answers){
+    if(answers[i]==qVar.questions[i].answer){
+      count++;
+    }
+  }
+  if(count>=Math.ceil(qVar.questions.length/2)){
+    document.getElementById("audio-win").play();
+    document.body.style.backgroundImage = "url('https://media.giphy.com/media/26tOZ42Mg6pbTUPHW/giphy.gif')";
+  }
+  else{
+    document.getElementById("audio-lose").play();
+    document.body.style.backgroundImage = "url('https://media.giphy.com/media/GWlO71pnRfZ9C/giphy.gif')";
+  }
+  document.getElementById("topModal").style.display = "none";
+  if(sessionStorage.getItem('topicNo')==quiz.topics.length-1) {document.getElementById("nextTop").innerHTML=" <span class='glyphicon glyphicon-forward'></span> "+quiz.topics[0].name;}
+  else document.getElementById("nextTop").innerHTML=" <span class='glyphicon glyphicon-forward'></span> "+quiz.topics[parseInt(sessionStorage.getItem('topicNo'))+1].name;
+  document.getElementsByClassName("modal-body1")[0].innerHTML="<p> You've scored "+count+" question(s) right!</p>";
+
+  var modal = document.getElementById('myModal');
+  modal.style.display = "block";
+}
+
+function saveExit(){
+ var topNo = sessionStorage.getItem('topicNo');
+ sessionStorage.setItem(qVar.name, answers);
+ sessionStorage.setItem("dist", timeLimit);
+ sessionStorage.setItem("questionNo",qno);
+ window.location.href = 'landing.html';
+}
+
+function nextTopic(){
+  if(sessionStorage.getItem('topicNo')==quiz.topics.length-1) {
+    sessionStorage.setItem('topicNo', 0);
+  }
+  else sessionStorage.setItem('topicNo', parseInt(sessionStorage.getItem('topicNo'))+1);
+  window.location.href = 'quiz.html';
+}
+
+// Clock
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+var minute = 0;
+radius = radius * 0.90;
+minute=-(Math.PI/179);
+drawFace(ctx, radius);
+drawNumbers(ctx, radius);
+
+function drawHand() {
+  minute+=(((2*Math.PI)/3)/60);
+  ctx.beginPath();
+  ctx.lineWidth = radius*0.035;
+  ctx.moveTo(0,0);
+  ctx.rotate(minute);
+  ctx.lineTo(0, -radius*0.8);
+  ctx.stroke();
+  ctx.rotate(-minute);
+}
+
+function drawFace(ctx, radius) {
+  var grad;
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2*Math.PI);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+  grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.1);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius*0.1;
+  ctx.stroke();
+  ctx.beginPath();
+  //ctx.arc(0, 0, radius*0.009, 0, 2*Math.PI);
+  //ctx.fillStyle = '#333';
+  //ctx.fill();
+  
+}
+
+function drawNumbers(ctx, radius) {
+  var ang = 0;
+  // ctx.font = radius*0.15 + "px arial";
+  // ctx.textBaseline="middle";
+  // ctx.textAlign="center";
+  for(num = 1; num < qVar.questions.length+1; num++){
+    ang += (2*Math.PI / qVar.questions.length);
+    ctx.rotate(ang);
+    ctx.translate(0, -radius*0.85);
+    //ctx.rotate(-ang);
+    ctx.moveTo(0,0);
+    ctx.lineTo(2,0);
+    ctx.stroke();
+    //ctx.rotate(ang);
+    ctx.translate(0, radius*0.85);
+    ctx.rotate(-ang);
+  }
+}
